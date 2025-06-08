93aea12b673a641b09a98a02c869eef99e6f8367
Naimish Omar <naimishomar@gmail.com>
2025-06-08 11:55:03 +0530

    update env

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ import contentRoute from './routes/content.route.js'
 dotenv.config();
 import cors from 'cors';
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
